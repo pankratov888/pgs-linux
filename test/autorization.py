985027da2335ee4156c360e04d0592817ace7f23
OFFICE <pankratov@evolenta.ru>
2024-07-23 19:23:07 +0300

    push 2

--- test/autorization.py
+++ test/autorization.py
@@ -33,27 +33,9 @@ browser = webdriver.Chrome(service=service, options=options)
 try:
     print("Открытие страницы...")
     browser.get("https://auth.psi.pgs.gosuslugi.ru/auth/realms/DigitalgovTorkndPsiAuth/protocol/openid-connect/auth?client_id=DigitalgovTorkndPsiAuth-Proxy&nonce=aa9422a9cfe770cdc50b7b5ef6af9005&state=bf4730d94618c5333d91b7949555208a&redirect_uri=https%3A%2F%2Fpsi.pgs.gosuslugi.ru%2Fopenid-connect-auth%2Fredirect_uri&scope=openid%20email&response_type=code")
+    # Вывод HTML страницы для отладки
+    print(browser.page_source)
     print("Страница загружена.")
-
-    # Найти поле поиска
-    search_box = browser.find_element(By.NAME, 'text')  # Поле поиска на Яндекс имеет имя 'text'
-
-    # Ввести текст в поле поиска
-    search_text = 'Привет, мир!'
-    search_box.send_keys(search_text)
-
-    # Проверка, что текст введен
-    entered_text = search_box.get_attribute('value')
-    if entered_text == search_text:
-        print("Текст успешно введен в поле поиска.")
-    else:
-        print("Ошибка: введенный текст не совпадает с ожидаемым.")
-
-    # Нажать кнопку поиска (обычно это кнопка с типом submit или может быть отдельная кнопка)
-    search_box.send_keys(Keys.RETURN)  # Нажать Enter для отправки формы
-
-    # Немного подождать, чтобы увидеть результаты поиска
-    time.sleep(3)  # Подождите 3 секунды (можно настроить время ожидания в зависимости от скорости вашего соединения)
 
 finally:
     # Закрыть браузер
