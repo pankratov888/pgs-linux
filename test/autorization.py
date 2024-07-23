65c70808a8d6732ae53932ac9b6817349e2590d4
OFFICE <pankratov@evolenta.ru>
2024-07-23 12:21:35 +0300

    push 2

--- test/autorization.py
+++ test/autorization.py
@@ -14,6 +14,7 @@ os.chmod(chromedriver_path, 0o755)
 
 # Настройки Chrome
 options = Options()
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
