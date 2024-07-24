c3430b93ef965a1a04074fb19bdc8435b94d8734
OFFICE <pankratov@evolenta.ru>
2024-07-24 12:43:45 +0300

    yandex 7

--- test/autorization.py
+++ test/autorization.py
@@ -2,6 +2,7 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,13 +17,18 @@ os.chmod(binary_yandex_driver_file, 0o755)  # Сделать файл испол
 #if not os.path.isfile(extension_path):
     #raise FileNotFoundError(f"Extension not found: {extension_path}")
 
-pem_path = './extensions/1.2.13_0.pem'
-
-# Настройки Chrome
+#pem_path = './extensions/1.2.13_0.pem'
 
 options = webdriver.ChromeOptions()
+
 service = ChromeService(executable_path=binary_yandex_driver_file)
 driver = webdriver.Chrome(service=service, options=options)
+wait = WebDriverWait(driver, 10)
+driver.implicitly_wait(10)
+options.add_argument('--headless')
+logs = driver.get_log('browser')
+network_logs = driver.get_log("browser")
+current_url = driver.current_url
 
 
 
