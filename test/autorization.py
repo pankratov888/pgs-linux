78039dc53d527bbb22a959c8423f44547500a417
OFFICE <pankratov@evolenta.ru>
2024-07-24 12:21:23 +0300

    yandex 6

--- test/autorization.py
+++ test/autorization.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.keys import Keys
 binary_yandex_driver_file = r'./bin/yandexdriver'
 os.chmod(binary_yandex_driver_file, 0o755)  # Сделать файл исполняемым
 # Путь к расширению
-extension_path = './extensions/1.2.13_0.crx'
+#extension_path = './extensions/1.2.13_0.crx'
 #if not os.path.isfile(extension_path):
     #raise FileNotFoundError(f"Extension not found: {extension_path}")
 
@@ -21,27 +21,21 @@ pem_path = './extensions/1.2.13_0.pem'
 # Настройки Chrome
 
 options = webdriver.ChromeOptions()
-options.headless = False
 service = ChromeService(executable_path=binary_yandex_driver_file)
 driver = webdriver.Chrome(service=service, options=options)
 
-options.add_extension(extension_path)
-options.add_argument(f'--ssl-client-cert={pem_path}')
 
-# Включение логирования в Chrome
-options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
 
-driver = None
 
-try:
-    print("Запуск браузера...")
-    driver = webdriver.Chrome(service=service, options=options)
-    print("Браузер запущен успешно.")
 
-    print("Открытие страницы...")
-    driver.get("https://auth.psi.pgs.gosuslugi.ru/auth/realms/DigitalgovTorkndPsiAuth/protocol/openid-connect/auth?client_id=DigitalgovTorkndPsiAuth-Proxy&nonce=aa9422a9cfe770cdc50b7b5ef6af9005&state=bf4730d94618c5333d91b7949555208a&redirect_uri=https%3A%2F%2Fpsi.pgs.gosuslugi.ru%2Fopenid-connect-auth%2Fredirect_uri&scope=openid%20email&response_type=code")
-    print("Страница загружена.")
+print("Запуск браузера...")
+driver = webdriver.Chrome(service=service, options=options)
+print("Браузер запущен успешно.")
 
-finally:
-    # Закрыть браузер
-    driver.quit()
+print("Открытие страницы...")
+driver.get("https://ya.ru")
+print("Страница загружена.")
+
+
+# Закрыть браузер
+driver.quit()
