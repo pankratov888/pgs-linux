7f4b4464879b5af4b184b7cf9d5d75210c2d749c
OFFICE <pankratov@evolenta.ru>
2024-07-24 14:54:42 +0300

    yandex 11

--- test/autorization.py
+++ test/autorization.py
@@ -16,9 +16,6 @@ os.chmod(chromedriver_path, 0o755)
 
 # Путь к расширению
 extension_path = './extensions/1.2.13_0.crx'
-if not os.path.isfile(extension_path):
-    raise FileNotFoundError(f"Extension not found: {extension_path}")
-
 pem_path = './extensions/1.2.13_0.pem'
 
 # Настройки Chrome
@@ -41,6 +38,7 @@ browser = webdriver.Chrome(service=service, options=options)
 
 print("Открытие страницы...")
 browser.get("https://auth.pgs.gosuslugi.ru/auth/realms/DigitalgovTorkndProd1Auth/protocol/openid-connect/auth?client_id=DigitalgovTorkndProd1Auth-Proxy&state=b6fa62fc48c9м04787fa5bf095da2bafa&nonce=8bf3d529b0af28816d18e97bf560c4d3&response_type=code&redirect_uri=https%3A%2F%2Fpgs.gosuslugi.ru%2Fopenid-connect-auth%2Fredirect_uri&scope=openid")
+time.sleep(3)
 print("Страница загружена.")
 
 
