655a1756dbfb007cd3d868e9b760c33de3461c11
OFFICE <pankratov@evolenta.ru>
2024-07-24 15:38:49 +0300

    google 4

--- test/autorization.py
+++ test/autorization.py
@@ -27,25 +27,7 @@ driver = webdriver.Chrome(service=service)
 print("Открытие страницы...")
 driver.get("http://ya.ru")
 print("Страница загружена.")
-# Найти поле поиска
-search_box = driver.find_element(By.NAME, 'text')  # Поле поиска на Яндекс имеет имя 'text'
-
-# Ввести текст в поле поиска
-search_text = 'Привет, мир!'
-search_box.send_keys(search_text)
-
-# Проверка, что текст введен
-entered_text = search_box.get_attribute('value')
-if entered_text == search_text:
-    print("Текст успешно введен в поле поиска.")
-else:
-    print("Ошибка: введенный текст не совпадает с ожидаемым.")
-
-# Нажать кнопку поиска (обычно это кнопка с типом submit или может быть отдельная кнопка)
-search_box.send_keys(Keys.RETURN)  # Нажать Enter для отправки формы
-
-# Немного подождать, чтобы увидеть результаты поиска
-time.sleep(3)  # Подождите 3 секунды (можно настроить время ожидания в зависимости от скорости вашего соединения)
-
+# Вывод HTML страницы для отладки
+print(driver.page_source)
 # Закрыть браузер
 driver.quit()
