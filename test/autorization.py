bdb4c6c79487c785767f3700caeec7ea9e39413d
OFFICE <pankratov@evolenta.ru>
2024-07-24 10:31:44 +0300

    push 2

--- test/autorization.py
+++ test/autorization.py
@@ -9,8 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
-
-
 # Установка прав на исполняемый файл ChromeDriver
 chromedriver_path = ChromeDriverManager().install()
 os.chmod(chromedriver_path, 0o755)
@@ -38,10 +36,12 @@ service = ChromeService(executable_path=chromedriver_path)
 browser = None
 
 try:
+    print("Запуск браузера...")
+    browser = webdriver.Chrome(service=service, options=options)
+    print("Браузер запущен успешно.")
+
     print("Открытие страницы...")
     browser.get("https://auth.psi.pgs.gosuslugi.ru/auth/realms/DigitalgovTorkndPsiAuth/protocol/openid-connect/auth?client_id=DigitalgovTorkndPsiAuth-Proxy&nonce=aa9422a9cfe770cdc50b7b5ef6af9005&state=bf4730d94618c5333d91b7949555208a&redirect_uri=https%3A%2F%2Fpsi.pgs.gosuslugi.ru%2Fopenid-connect-auth%2Fredirect_uri&scope=openid%20email&response_type=code")
-    # Вывод HTML страницы для отладки
-    print(browser.page_source)
     print("Страница загружена.")
 
 finally:
