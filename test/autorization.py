7ad15d923531072680037afe775178638e5a170d
OFFICE <pankratov@evolenta.ru>
2024-07-24 13:00:22 +0300

    yandex 8

--- test/autorization.py
+++ test/autorization.py
@@ -33,5 +33,25 @@ browser = webdriver.Chrome(service=service, options=options)
 print("Открытие страницы...")
 browser.get("http://ya.ru")
 print("Страница загружена.")
+# Найти поле поиска
+search_box = browser.find_element(By.NAME, 'text')  # Поле поиска на Яндекс имеет имя 'text'
+
+# Ввести текст в поле поиска
+search_text = 'Привет, мир!'
+search_box.send_keys(search_text)
+
+# Проверка, что текст введен
+entered_text = search_box.get_attribute('value')
+if entered_text == search_text:
+    print("Текст успешно введен в поле поиска.")
+else:
+    print("Ошибка: введенный текст не совпадает с ожидаемым.")
+
+# Нажать кнопку поиска (обычно это кнопка с типом submit или может быть отдельная кнопка)
+search_box.send_keys(Keys.RETURN)  # Нажать Enter для отправки формы
+
+# Немного подождать, чтобы увидеть результаты поиска
+time.sleep(3)  # Подождите 3 секунды (можно настроить время ожидания в зависимости от скорости вашего соединения)
+
 # Закрыть браузер
 browser.quit()
