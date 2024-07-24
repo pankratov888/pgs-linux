7052061f7e81611b10c9387f1e6ed820098e0f12
OFFICE <pankratov@evolenta.ru>
2024-07-24 12:09:50 +0300

    yandex 5

--- test/autorization.py
+++ test/autorization.py
@@ -21,15 +21,10 @@ pem_path = './extensions/1.2.13_0.pem'
 # Настройки Chrome
 
 options = webdriver.ChromeOptions()
+options.headless = False
 service = ChromeService(executable_path=binary_yandex_driver_file)
 driver = webdriver.Chrome(service=service, options=options)
 
-#options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--disable-extensions")
 options.add_extension(extension_path)
 options.add_argument(f'--ssl-client-cert={pem_path}')
 
