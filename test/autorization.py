591d2cf8be06f816dffebec1e86c5ae69fa7d421
OFFICE <pankratov@evolenta.ru>
2024-07-24 11:37:23 +0300

    yandex 3

--- test/autorization.py
+++ test/autorization.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
-binary_yandex_driver_file = r'./bim/yandexdriver'
+binary_yandex_driver_file = r'./bin/yandexdriver'
 # Путь к расширению
 extension_path = './extensions/1.2.13_0.crx'
 #if not os.path.isfile(extension_path):
@@ -23,7 +23,7 @@ options = webdriver.ChromeOptions()
 service = ChromeService(executable_path=binary_yandex_driver_file)
 driver = webdriver.Chrome(service=service, options=options)
 
-#options.add_argument("--headless=new")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
