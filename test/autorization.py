d7b7230628ac5e69bc6b4fae86391dc22b0caff0
OFFICE <pankratov@evolenta.ru>
2024-07-25 15:07:28 +0300

    tor 12

--- test/autorization.py
+++ test/autorization.py
@@ -24,55 +24,31 @@ driver = webdriver.Chrome(options=chrome_options, service=service)
 
 wait = WebDriverWait(driver, 10)
 
-print("Открытие страницы...")
-driver.get("https://demo.knd.gov.ru/login")
-print("Страница загружена.")
-time.sleep(5)
+try:
+    print("Открытие страницы...")
+    driver.get("http://ya.ru")
+    print("Страница загружена.")
 
+    # Найти поле поиска
+    search_box = driver.find_element(By.NAME, 'text')  # Поле поиска на Яндекс имеет имя 'text'
 
-# Нажимаем кнопку "Вход через ЕСИА"
-next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"/html/body/app-root/evolenta-login/div/div[2]/div/div")))
-next_button.click()
-# Ждем, чтобы страница ЕСИА загрузилась
-driver.implicitly_wait(2)
-# Активация поля ввода телефона и ввод телефона
-login_input = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/esia-root/div/esia-login/div/div[1]/form/div[1]/esia-input/input")))
-login_input = driver.find_element("xpath", "/html/body/esia-root/div/esia-login/div/div[1]/form/div[1]/esia-input/input")
-login_input.click()  # Активация поля ввода
-time.sleep(2)
-login_input.send_keys("+79374426231")  # Ввод телефона
-# Активация поля ввода пароля и ввод пароля
-login_input = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/esia-root/div/esia-login/div/div[1]/form/div[2]/esia-input-password/div/input")))
-password_input = driver.find_element("xpath", "/html/body/esia-root/div/esia-login/div/div[1]/form/div[2]/esia-input-password/div/input")
-password_input.click()  # Активация поля ввода
-time.sleep(2)
-password_input.send_keys("S.pank470")  # Ввод пароля
-# Нажимаем кнопку "Войти"
-login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/esia-root/div/esia-login/div/div[1]/form/div[4]/button")))
-login_button.click()
-# Открываем сервис генерации TOTP-кодов в новом окне
-driver.execute_script("window.open('https://piellardj.github.io/totp-generator/?secret=AFDQSZB3NFBUCTBRSUEZ6NWCQIWCR66S&digits=6&period=30&algorithm=SHA-1')")
-driver.switch_to.window(driver.window_handles[1])
-# Находим и копируем код
-copy_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/main/div[3]/div/div[2]/div[1]/button")))
-copy_button.click()
-time.sleep(1)
-# Переходим обратно в первое окно
-driver.switch_to.window(driver.window_handles[0])
-# Вставляем скопированный код в поле ввода на странице авторизации
-code_input = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/esia-root/div/esia-login/div/div/esia-enter-mfa/esia-ttp/form/div[2]/div/esia-code-input/div/code-input/span[1]/input")))
-code_input.click()
-time.sleep(1)
-code_input.send_keys(Keys.CONTROL + 'v')  # Вставляем скопированный код
-# Задержка перед переходом на следующую страницу
-time.sleep(5)  # Подождать 5 секунд, чтобы страница загрузилась
-driver.refresh()
-# Нажимаем кнопку "Далее"
-next_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/evolenta-select-branch/div/div[2]/div[2]/div/div/div[2]")))
-next_button.click()
-# Ждем, пока страница загрузится
-time.sleep(5)  # Подождать 5 секунд
+    # Ввести текст в поле поиска
+    search_text = 'Привет, мир!'
+    search_box.send_keys(search_text)
 
+    # Проверка, что текст введен
+    entered_text = search_box.get_attribute('value')
+    if entered_text == search_text:
+        print("Текст успешно введен в поле поиска.")
+    else:
+        print("Ошибка: введенный текст не совпадает с ожидаемым.")
 
-# Закрыть браузер
-driver.quit()
+    # Нажать кнопку поиска (обычно это кнопка с типом submit или может быть отдельная кнопка)
+    search_box.send_keys(Keys.RETURN)  # Нажать Enter для отправки формы
+
+    # Немного подождать, чтобы увидеть результаты поиска
+    time.sleep(3)  # Подождите 3 секунды (можно настроить время ожидания в зависимости от скорости вашего соединения)
+
+finally:
+    # Закрыть браузер
+    driver.quit()
