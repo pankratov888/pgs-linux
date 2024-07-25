00840a6ab48e9e719c4ad75d07d521f520bab002
OFFICE <pankratov@evolenta.ru>
2024-07-25 12:18:04 +0300

    tor 9

--- test/autorization.py
+++ test/autorization.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from pyvirtualdisplay import Display
+
 
 
 
